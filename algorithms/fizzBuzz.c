9e88217702f230322c5546b71cedd1f65d73339d
anerdsmeltybrain <hdviews.01@gmail.com>
2026-05-23 23:55:26 -0400

    fizz buzz

---
+++ algorithms/fizzBuzz.c
@@ -0,0 +1,17 @@
+/* fizz buzz */
+#include <stdio.h>
+
+int main(void) {
+
+  for(int i = 0; i < 101; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      printf("%d - FizzBuzz\n", i);
+    } else if ( i % 5 == 0 ) {
+      printf("%d - Fizz\n", i);
+    } else if ( i % 3 == 0 ) {
+      printf("%d - Buzz\n", i);
+    }
+  }
+
+  return 0;
+}
